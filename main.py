6602bac47205553a378d79370eec5ee1812d505b
Silletr <hristicenkoigor764@gmail.com>
2025-08-07 09:48:26 +0000

    [NEW FILE/DIR: TODO.md, CHANGED DIR/FILE: main.py, requirements.txt, RENAMED FILE/DIR: game_process/hands.py -> game_process/player.py] Added TODO file for convenience, renamed file for better readability, rewrited code for Discord.py library instead Telegram aiogram3.x

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-# --- IMPORTS
+# --- IMPORTS ---
 import sys
 from os import getenv
 
 from game_process import *
 
-# --- 
+import discord 
+from discord.ext import commands
 
-# --- VARIABLES AND .ENV
+# --- VARIABLES AND .ENV ---
 TOKEN = getenv("JACK_TOKEN")
-# ---
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None) 
 
 
-# --- Start command
+# --- START COMMAND ---
+@bot.command()
+async def start(ctx):
+    await ctx.send('Hello from LazyDeveloper community!')
+
+# -- HELP COMMAND ---
+@bot.command()
+async def help(ctx):
+    await ctx.send(
+        "So, you need help, right? Ill give you this.\n"
+        "!help - well you understood, right?\n"
+        "!blackjack - start game with dealer (algorithm, not a real human)\n"
+        "!ping - pong!\n"
+        "Bot creator - @silletr_wt\n"
+        "Username of creator in Telegram - @Python_tor"
+    )
+
+# --- PING COMMAND ---
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+# --- BLACKJACK COMMAND ---
+@bot.command()
+async def blackjack(ctx):
+    pass
 
 
-# --- point of enter
+# --- POINT OF ENTER  ---
+def main():
+    bot.run(TOKEN)
 
-# ---
+if __name__ == "__main__":
+    main()
