14c613dea7b35835cb80070185240ca3eef62b89
Silletr <hristicenkoigor764@gmail.com>
2025-08-07 07:57:30 +0000

    modified:   main.py 	deleted:    requirements.txt

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
 # --- IMPORTS
-import asyncio
-import logging
 import sys
 from os import getenv
 
 from game_process import *
 
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
 # --- 
 
 # --- VARIABLES AND .ENV
 TOKEN = getenv("JACK_TOKEN")
-dp = Dispatcher()
 # ---
 
 
 # --- Start command
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {message.from_user.user_name}!")
+
 
 # --- point of enter
-async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    # And the run events dispatching
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
 # ---
